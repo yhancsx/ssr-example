9451f5b787ab48aedb50af97d25ecbafad52a0b4
johnbae93 <johnbae57@naver.com>
2020-09-08 14:40:42 +0900

    counter function in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+      <div>
+        Home
+        <div>counter: {count}</div>
+        <button type="button" onClick={() => setCount(prev => prev + 1)}>
+          increase
+        </button>
+        <button type="button" onClick={() => setCount(prev => prev - 1)}>
+          decrease
+        </button>
+      </div>
+  );
+};
 
 export default Home;
